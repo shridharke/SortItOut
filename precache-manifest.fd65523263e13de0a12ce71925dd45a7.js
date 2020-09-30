800d8ef169eff4cf53bef49774817926fe3620f7
Shridhar <shridharelango@gmail.com>
2020-09-30 16:50:57 +0530

    Updates

---
+++ precache-manifest.fd65523263e13de0a12ce71925dd45a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303ee252c6227734952fe98d6c7c6eaf",
+    "url": "/sort-it-out/index.html"
+  },
+  {
+    "revision": "02b1ebe60c9189a061cd",
+    "url": "/sort-it-out/static/css/main.8728fcfe.chunk.css"
+  },
+  {
+    "revision": "87910b9fe0d4a503a9af",
+    "url": "/sort-it-out/static/js/2.e5108450.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sort-it-out/static/js/2.e5108450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b1ebe60c9189a061cd",
+    "url": "/sort-it-out/static/js/main.73b33faf.chunk.js"
+  },
+  {
+    "revision": "6bfdb22c0793aa3e8d0d",
+    "url": "/sort-it-out/static/js/runtime-main.38a7050a.js"
+  }
+]);
